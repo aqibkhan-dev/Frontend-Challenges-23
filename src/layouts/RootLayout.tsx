46c40b1f3d718aacc9bd77e764d57dbb7113336d
Aqib khan <Aqibkhan-dev@gmail.com>
2024-10-25 18:57:48 +0530

    Frontend Mentor's 23th challenge is over

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Home from "./Home"
+
+
+const RootLayout = () => {
+  return (
+    <>
+    <Home />
+    <Outlet />
+    </>
+  );
+};
+
+export default RootLayout
